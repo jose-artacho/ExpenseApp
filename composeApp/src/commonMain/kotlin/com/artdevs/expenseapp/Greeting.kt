4d55b6c22620ccbe2e6d6ae52f4915763c0a9c63
Jose Artacho <jagartacho@gmail.com>
2025-01-11 15:30:52 +0100

    F02 - Added main dependencies

--- composeApp/src/commonMain/kotlin/com/artdevs/expenseapp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.artdevs.expenseapp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/artdevs/expenseapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.artdevs.expenseapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
